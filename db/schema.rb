bf6b3a11ec32c403fa98a937c06199446df38ae9
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2023-12-20 17:58:33 +0100

    Feat - Setup user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_090353) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_20_163602) do
   create_table "links", force: :cascade do |t|
     t.string "target"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
