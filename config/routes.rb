848222d1c404c9bacdc7d9be34c8a987a801458a
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2023-12-22 16:43:52 +0100

    Feat - Route, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :links, only: %i[index create]
   resources :users, only: %i[index create]
+  resources :products, only: %i[index]
 end
