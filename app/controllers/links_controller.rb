c5fc2c9cae9bb02acdeb68c5f1d918001bcd693c
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2024-11-29 17:56:23 +0100

    Feat - Improvement for XSS risk

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,27 +5,13 @@ class LinksController < ApplicationController
   end
 
   def create
-    unless link_params[:target].start_with?("javascript")
-      flash[:notice] = message
-      redirect_to links_path and return
-    end
-
-    Link.create(link_params)
+    target = link_params[:target].start_with?('www') ? "https://#{link_params[:target]}" : link_params[:target]
+    Link.create(target:)
 
     redirect_to links_path
   end
 
   private
-
-  def message
-    '
-    For this exercise please submit a JS injection like:
-    javascript: (function() {
-      var randomColor = Math.floor(Math.random()*16777215).toString(16);
-      document.body.style.backgroundColor = "#" + randomColor;
-    })();
-    '
-  end
 
   def link_params
     params.require(:link).permit(:target)
